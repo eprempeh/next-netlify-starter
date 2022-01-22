0556da638edc4f4ae91d3bd800e374a92b84cde8
Kojo Prempeh <eprempeh19@gmail.com>
2022-01-22 21:03:10 +0000

    Change to my name

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kojo Prempeh</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Kojo Prempeh</code>
         </p>
       </main>
 
